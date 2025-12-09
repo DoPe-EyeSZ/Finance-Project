938565e0b260a1a75e76315f3f5e909b724af1bc
Ryan Yeung <ryeung0305@gmail.com>
2025-12-09 10:12:19 -0800

    Use Flask-Migrate for Railway initialization

--- init_db.py
+++ init_db.py
@@ -1,7 +1,8 @@
 from app import create_app, db
+from flask_migrate import upgrade
 
 app = create_app()
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    upgrade()  
+    print("Database migrations applied successfully!")
